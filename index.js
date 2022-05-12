d348cc1adb4f2c1f10ea9de25ca5a05b7ada2209
SampleUser <pedro.cunha@onfinity.com.br>
2022-05-11 21:22:41 -0300

    Mosularizando códigO

--- index.js
+++ index.js
@@ -6,31 +6,33 @@ async function readFile(filePath) {
     return await fs.promises.readFile(filePath, 'utf8')
 }
 
+function stringToObject(string) {
+    let commaSepareted = string.split(',') // ['00/00/00', ' 22:43 - User: message']
+    let date = commaSepareted[0] // 00/00/00
+    if(commaSepareted.length >= 2){
+
+        let hyphenSepareted = commaSepareted.filter((_, index) => index >= 1).join().split('-')
+        let time = hyphenSepareted[0].trimEnd().trimStart()
+
+        let twoPointsSepareted = hyphenSepareted.filter((_, index) => index >= 1).join().split(':')
+        let username = twoPointsSepareted[0].trimEnd().trimStart()
+        let message = twoPointsSepareted.filter((_, index) => index >= 1).join()
+
+        return {
+            date,
+            time,
+            username,
+            message
+        }
+    }
+    return null
+}
+
 async function main() {
     console.log("=========== WHATSAPP-BACKUP-READER ===========")
     const text = await readFile('./files/whats.txt')
     const lines = text.split('\n')
-    const messages = lines.map(line => {
-        let commaSepareted = line.split(',') // ['00/00/00', ' 22:43 - User: message']
-        let date = commaSepareted[0] // 00/00/00
-        if(commaSepareted.length >= 2){
-
-            let hyphenSepareted = commaSepareted.filter((_, index) => index >= 1).join().split('-')
-            let time = hyphenSepareted[0].trimEnd().trimStart()
-
-            let twoPointsSepareted = hyphenSepareted.filter((_, index) => index >= 1).join().split(':')
-            let username = twoPointsSepareted[0].trimEnd().trimStart()
-            let message = twoPointsSepareted.filter((_, index) => index >= 1).join()
-
-            return {
-                date,
-                time,
-                username,
-                message
-            }
-        }
-        return null
-    })
+    const messages = lines.map(line => stringToObject(line)).filter(line => line)
     console.log(messages)
 }
 main()
