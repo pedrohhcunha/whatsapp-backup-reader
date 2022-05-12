6086d4ec8f6a670d04872a4ca53cb9ac9225032c
SampleUser <pedro.cunha@onfinity.com.br>
2022-05-11 21:49:36 -0300

    Versão 100% funcional

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const fs = require('fs')
 
 async function readFile(filePath) {
-    console.log(`Reading file: ${filePath}`)
-
     return await fs.promises.readFile(filePath, 'utf8')
 }
 
@@ -28,11 +26,28 @@ function stringToObject(string) {
     return null
 }
 
+async function writeJSON(listOfMessages) {
+    return await fs.promises.writeFile(`./outputs/${new Date().valueOf()}.json`, JSON.stringify(listOfMessages))
+}
+
+async function writeCSV(listOfMessages) {
+    let csv = 'date,time,username,message\n'
+    listOfMessages.forEach(message => {
+        csv += `${message.date},${message.time},${message.username},${message.message}\n`
+    })
+    return await fs.promises.writeFile(`./outputs/${new Date().valueOf()}.csv`, csv)
+}
+
 async function main() {
-    console.log("=========== WHATSAPP-BACKUP-READER ===========")
-    const text = await readFile('./files/whats.txt')
+    console.time("WhatsApp-Backup-Reader")
+    const text = await readFile('./inputs/whats.txt')
     const lines = text.split('\n')
-    const messages = lines.map(line => stringToObject(line)).filter(line => line)
-    console.log(messages)
+    const messagesList = lines.map(line => stringToObject(line)).filter(line => line)
+    const writingResults = await Promise.all([
+        writeJSON(messagesList),
+        writeCSV(messagesList)
+    ])
+    console.timeEnd("WhatsApp-Backup-Reader")
+    return writingResults.every(result => result)
 }
 main()
